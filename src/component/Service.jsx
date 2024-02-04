58df77674859c08ff999217db5f5bf273007e39e
AMAN KUMAR <aman2110113@akgec.ac.in>
2024-02-04 19:33:42 +0530

    Add option to service section

--- src/component/Service.jsx
+++ src/component/Service.jsx
@@ -1,52 +1,41 @@
 import React from "react";
-import react from "../assets/react.svg"
+import ReactLogo from "../assets/react.svg"
+import NodeLogo from "../assets/node.svg"
 const Service = () => {
+
+  const servicesOption= [
+
+    {
+      id:1,
+      title:"Front-End Development",
+      des:"Turn what you have in mind of a digital product into reality. For any platform you consider",
+      img:ReactLogo
+
+    },
+    {
+      id:2,
+      title:"Backend-End Development",
+      des:"Standard designing, building, and implementing your applications with documentation",
+      img:NodeLogo
+
+    },
+    {
+      id:3,
+      title:"Application Development",
+      des:"Create and maintain your websites and also take care of its performance and traffic capacity.",
+      img:ReactLogo
+
+    }
+  ]
+
+  console.log(servicesOption)
+
   return (
     <div id="service">
-      <div className="section-title5">
-        <b className="title3">Specialized in</b>
-        <div className="above-title5">SERVICES</div>
-      </div>
+      <div className="section-title">
+        <b className="specialized">Specialized in</b>
+        <div className="above-title">SERVICES</div>
 
-      <div className="services">
-        <div className="service-item">
-          <div className="icon3">
-            <img className="react" alt="" src={react} />
-          </div>
-          <div className="title14">
-            <h2>Front-End Development</h2>
-          </div>
-          <div className="description1">
-            Turn what you have in mind of a digital product into reality. For
-            any platform you consider.
-          </div>
-        </div>
-
-        <div className="service-item">
-          <div className="icon3">
-            <img className="pin-icon" alt="" src="/app.svg" />
-          </div>
-          <div className="title15">
-            <h2>Back-End  Development</h2>
-          </div>
-          <div className="description1">
-            Standard designing, building, and implementing your applications
-            with documentation.
-          </div>
-        </div>
-
-        <div className="service-item">
-          <div className="icon3">
-            <img className="pin-icon" alt="" src="/globe.svg" />
-          </div>
-          <div className="title16">
-            <h2>Application Development</h2>
-          </div>
-          <div className="description1">
-            Create and maintain your websites and also take care of its
-            performance and traffic capacity.
-          </div>
-        </div>
       </div>
     </div>
   );
