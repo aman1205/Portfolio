bd2c549d84c9033da053a911b4471a6ee4780574
AMAN KUMAR <aman2110113@akgec.ac.in>
2024-02-08 22:41:19 +0530

    update  main config

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Header from "./component/Header";
+import Home from "./pages/Home";
+import "./styles/app.scss"
+// import "./styles/header.scss"
+// import "./styles//home.scss"
+// import "./styles//service.scss"
+// import "./styles/Contact.scss"0
+// import './styles/Project.scss'
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Home/>  
+      {/* <Service/>  */}
+      {/* <Projects/> */}
+      {/* <Contact/> */}
+      {/* <Footer/> */}
+      
+    </>
+  );
+}
+
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js}"],
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#FFFFFF',
+        secondary: '#000000',
+        main: '#1E1E1E',
+        box: '#F1F1F1B2'
+      },
+      fontFamily: {
+        robotoFont: ['Roboto', 'sans-serif'],
+        openSansFont: ['Open Sans', 'sans-serif'],
+      },
+
+    },
+    
   },
+  darkMode:"class",
+  variants: {},
   plugins: [],
 }
