3cc958b3cccdf6496fe242735504bafeb5d14c3e
AMAN KUMAR <aman2110113@akgec.ac.in>
2024-02-20 14:05:45 +0530

    switch theme based on system

--- src/context/themeContext.jsx
+++ src/context/themeContext.jsx
@@ -3,7 +3,9 @@ import React, { createContext,useState } from 'react';
 export const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-  const [isDarkTheme, setIsDarkTheme] = useState("light");
+  const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
+  const value= darkThemeMq.matches ? 'dark' : 'light'; 
+  const [isDarkTheme, setIsDarkTheme] = useState(value);
 
   return (
     <ThemeContext.Provider value={{ isDarkTheme, setIsDarkTheme }}>
